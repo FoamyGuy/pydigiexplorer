abf3a40961392d4c156d5704127c6a83be1ee712
android_ninja <foamyguy@gmail.com>
2018-01-16 18:39:03 -0600

    finishing HTTP wrappers. adding websocket support

---
+++ pydigiexplorer-websocket.py
@@ -0,0 +1,86 @@
+import json
+import time
+from pprint import pprint
+import requests
+
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import websocket
+
+import pydigiexplorer as digi
+
+
+def ws_listen(on_transcation, on_block):
+    def ping(*args):
+        time.sleep(30)
+        print(">>> Ping...")
+        ws.send('2')
+
+    def subscribe(*args):
+        ws.send('2probe')
+
+    def on_message(ws, message):
+        #print("message received:\n{}".format(message))
+
+        if message == "3probe":
+            print("Subscribing to inv...")
+            ws.send("5")
+            ws.send('425["subscribe", "inv"]')
+
+        if message == '42["subscribed"]':
+            thread.start_new_thread(ping, ())
+
+        if message == "3":
+            print("<<< Pong...")
+            thread.start_new_thread(ping, ())
+
+        if '["block",' in message:
+            hash = message.replace('42["block","', '')
+            hash = hash.replace('"]', '')
+            if on_block is not None:
+                on_block(hash)
+            #pprint(digi.block(hash))
+
+        if '["tx"' in message:
+            tx_info = message.replace('42["tx",', '')
+            tx_info = tx_info.replace(']', '')
+            if on_transcation is not None:
+                on_transcation(json.loads(tx_info))
+            #print(txid)
+
+    def on_error(ws, error):
+        print("error")
+        print(error)
+
+    def on_close(ws):
+        print("### Socket Closed ###")
+
+    def on_open(ws):
+        print("### Socket Opened ###")
+        thread.start_new_thread(subscribe, ())
+
+    millis = int(round(time.time() * 1000))
+    r = requests.get("https://digiexplorer.info/socket.io/?EIO=2&transport=polling&t={}-0".format(millis))
+    sid = r.headers['Set-Cookie'].split('io=')[1]
+    websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        "wss://digiexplorer.info/socket.io/?EIO=2&transport=websocket&sid={}".format(sid),
+        on_message=on_message,
+        on_error=on_error,
+        on_close=on_close
+    )
+    ws.on_open = on_open
+    ws.run_forever()
+
+
+if __name__ == "__main__":
+    def on_block(hash):
+        print("block: %s" % hash)
+
+
+    def on_transaction(tx_info):
+        pprint(tx_info)
+
+    ws_listen(on_transaction, on_block)
--- pydigiexplorer.py
+++ pydigiexplorer.py
@@ -51,10 +51,48 @@ def addresses_utxo(addrs):
     print(addrs_str)
     return json.loads(requests.get(API_URL + "addrs/%s/utxo/" % addrs_str).content.decode("utf-8"))
 
+def sync_info():
+    return json.loads(requests.get(API_URL + "sync/").content.decode("utf-8"))
+
+def peer_info():
+    return json.loads(requests.get(API_URL + "peer/").content.decode("utf-8"))
+
+def status_info():
+    return json.loads(requests.get(API_URL + "status/?q=getInfo").content.decode("utf-8"))
+
+def status_difficulty():
+    return json.loads(requests.get(API_URL + "status/?q=getDifficulty").content.decode("utf-8"))
+
+def status_best_block_hash():
+    return json.loads(requests.get(API_URL + "status/?q=getBestBlockHash").content.decode("utf-8"))
+
+def status_last_block_hash():
+    return json.loads(requests.get(API_URL + "status/?q=getLastBlockHash").content.decode("utf-8"))
 
 
 
 #pprint(blocks(limit=10, blockDate="2014-01-10"))
 #pprint(addresses_utxo(["DJR9SuYSZMs353xuV4nj5Ro6cmFQ2iphtw", "DNHX9Ae6ZxyVVNNt9NtqVbMRMKBsxKbfXM"]))
-pprint(transactions_for_multiple_addresses(["DJR9SuYSZMs353xuV4nj5Ro6cmFQ2iphtw", "DNHX9Ae6ZxyVVNNt9NtqVbMRMKBsxKbfXM"]))
+#pprint(transactions_for_multiple_addresses(["DJR9SuYSZMs353xuV4nj5Ro6cmFQ2iphtw", "DNHX9Ae6ZxyVVNNt9NtqVbMRMKBsxKbfXM"]))
+#pprint(block("5136e9a8962bd7ca4deb887135d353fab7e74d40faaddf7e9410aa7065247f03"))
+
+#pprint(addresses_utxo(["DT19owMp6fXWJWxTASg62KAXUcNwQwSJtN", "DJnqfjzRFUYHoUEgKWhptxB73cGshfHaPe"]))
+#pprint(status_last_block_hash())
+
+#pprint(block("21f98a20612b75d63e68cdf11f196f68e16b0d29d0ac4c8e75a2fe11ec5afae8"))
 #pprint(block_index(1191987))
+
+#pprint(address_utxo("DU3GN9DHPBFwAnZbk5YDhLmXZZi2599BYC"))
+
+"""
+Javascript socket implementation
+var socket = io("http://<insight-server>:<port>/");
+socket.on('connect', function() {
+  // Join the room.
+  socket.emit('subscribe', room);
+})
+socket.on(eventToListenTo, function(data) {
+  console.log("New transaction received: " + data.txid)
+})
+"""
+
