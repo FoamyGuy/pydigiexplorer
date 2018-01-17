c7280c33c6ff583b361937f9ff2106b9b8ed8321
android_ninja <foamyguy@gmail.com>
2018-01-16 19:11:35 -0600

    examples

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # pydigiexplorer
 ---
 A python interface for fetching data from https://digiexplorer.info
+
+Usage: See [pydigiexplorer_example.py]()
---
+++ pydigiexplorer_example.py
@@ -0,0 +1,54 @@
+import pydigiexplorer as digi
+
+print(digi.block("5136e9a8962bd7ca4deb887135d353fab7e74d40faaddf7e9410aa7065247f03"))
+
+#print(digi.block_index(12345))
+
+#print(digi.blocks(limit=10, blockDate="2014-01-10"))
+
+#print(digi.transaction("a06c5affb6bbe97b18b844e746c8b6346cd1d091afb18910f822898b7d76b6df"))
+
+#print(digi.transactions(block_hash="5136e9a8962bd7ca4deb887135d353fab7e74d40faaddf7e9410aa7065247f03"))
+
+#print(digi.transactions(address="D5om677b21VUAFBkVsc4SwngAr16dBKbh6"))
+
+#print(digi.transactions_for_multiple_addresses(["D5om677b21VUAFBkVsc4SwngAr16dBKbh6", "DQWCMaEwxEQupNEBvWGMELwMPFxruZXgP5"]))
+
+#print(digi.address("D5om677b21VUAFBkVsc4SwngAr16dBKbh6"))
+
+#print(digi.address_balance("DQWCMaEwxEQupNEBvWGMELwMPFxruZXgP5"))
+
+#print(digi.address_total_received("DQWCMaEwxEQupNEBvWGMELwMPFxruZXgP5"))
+
+#print(digi.address_total_sent("DQWCMaEwxEQupNEBvWGMELwMPFxruZXgP5"))
+
+#print(digi.address_unconfirmed_balance("DQWCMaEwxEQupNEBvWGMELwMPFxruZXgP5"))
+
+#print(digi.address_utxo("D5om677b21VUAFBkVsc4SwngAr16dBKbh6"))
+
+#print(digi.addresses_utxo(["D5om677b21VUAFBkVsc4SwngAr16dBKbh6", "DQWCMaEwxEQupNEBvWGMELwMPFxruZXgP5"]))
+
+#print(digi.sync_info())
+
+#print(digi.status_last_block_hash())
+
+#print(digi.peer_info())
+
+#print(digi.status_info())
+
+#print(digi.status_difficulty())
+
+#print(digi.status_best_block_hash())
+
+#print(digi.status_last_block_hash())
+
+
+
+
+
+
+
+
+
+
+
